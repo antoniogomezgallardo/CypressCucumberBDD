6ffdb63fe9428eca4fc6a2d53e88c11fdf355e03
Antonio Gómez Gallardo <antoniogomezgallardo@MacBook-Pro-de-Antonio.local>
2024-05-12 06:43:18 +0200

    Removed the code for Mocha reports and added Cucumber reports

--- cypress.config.js
+++ cypress.config.js
@@ -14,13 +14,15 @@ async function setupNodeEvents(on, config) {
   on("file:preprocessor", preprocessor(config));
 
   // MochaAwesome reporter
-  require('cypress-mochawesome-reporter/plugin')(on)
+  //require('cypress-mochawesome-reporter/plugin')(on)
   // Make sure to return the config object as it might have been modified by the plugin.
   return config;
 }
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  //reporter: 'cypress-mochawesome-reporter', 
+  //--------> also need to paste this in package.json inside devDependencies:"cypress-mochawesome-reporter": "^3.8.2"
+
   video: true,
   e2e: {
     specPattern: "cypress/e2e/features/*.feature",
